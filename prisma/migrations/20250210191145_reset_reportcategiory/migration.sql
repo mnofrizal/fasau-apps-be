da11a26ddc5bcff257648fcd1648ce6b35be9944
mnofrizal <mnofrizal1996@gmail.com>
2025-02-11 02:11:53 +0700

    ok

---
+++ prisma/migrations/20250210191145_reset_reportcategiory/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [AC,MK,TIJ,TDP,JSI,PMT,PST] on the enum `ReportCategory` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "ReportCategory_new" AS ENUM ('CM', 'PM');
+ALTER TABLE "task_reports" ALTER COLUMN "category" TYPE "ReportCategory_new" USING ("category"::text::"ReportCategory_new");
+ALTER TYPE "ReportCategory" RENAME TO "ReportCategory_old";
+ALTER TYPE "ReportCategory_new" RENAME TO "ReportCategory";
+DROP TYPE "ReportCategory_old";
+COMMIT;
